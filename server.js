0f9aeeade49663f67e4fa272c8a5656f8f531ff2
Holt Moore <holt.w.moore@gmail.com>
2023-07-26 15:24:25 -0500

    final

--- server.js
+++ server.js
@@ -2,26 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 const budget = require('./models/budget'); 
-
-
 var bodyParser = require('body-parser');
 
+let bankAccount = 0;
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(bodyParser.json())
 
-
 app.get('/budgets', (req, res) => {
-    res.render('index', { budget: budget });
+    res.render('index', { budget: budget, bankAccount: bankAccount });
 });
 
 app.get('/budgets/new', (req, res) => {
-    // Render 'new' view
     res.render('new');
 });
 
@@ -34,11 +28,9 @@ app.post('/budgets', (req, res) => {
     console.log(req.body);
     req.body.tags = req.body.tags.split(',').map(tag => tag.trim());
     budget.push(req.body);
+    bankAccount += Number(req.body.amount);
     res.redirect('/budgets');
 });
-
-
-
 
 app.listen(PORT, () => {
     console.log(`PORT: ${PORT}, Budgtr running`);
